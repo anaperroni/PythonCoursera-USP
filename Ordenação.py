10a197ce060a717cdc649452ccdcc2db6c5d08a8
Ana Julia Perroni <anaperroni0403@gmail.com>
2025-04-27 17:09:08 -0300

    Exercicios - While e Repetições

---
+++ Ordenação.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+c = int(input("Digite um número: "))
+
+if a < b < c:
+    print("crescente")
+else:
+    print("Não está em ordem crescente")
---
+++ numeroNatural-Ex2.py
@@ -0,0 +1,7 @@
+n = int(input("Digite o valor de n: "))
+
+impar = 1
+
+for i in range(n):
+    print(impar)
+    impar += 2
